85c0a5f6e5377d76383b9ada8cd857769f48df27
Murilo De Sousa Gonçalves <40380133+Muri01@users.noreply.github.com>
2022-12-14 09:17:17 -0300

    Revert "semana 1 feita (sem rend condicional)"

--- pokedex/src/Components/Header/HeaderStyle.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components/';
-
-export const HeaderContainer = styled.header`
-    border: 1px solid black;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    padding: 10px
-`;
