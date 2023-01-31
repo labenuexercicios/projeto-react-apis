48907d493f0f08e3b4e1a7a1d979f16015788b8c
rogerio kayronny <kayronnysales@gmail.com>
2023-01-31 20:51:34 -0300

    adicionando componentes de card e navbar com ui materil

---
+++ app-pokedex-api/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavBar from '../components/NavBar';
+import PokemonCard from '../components/PokemonCard';
+
+export const Home = () => {
+  return (
+    <div>
+        <NavBar/>
+    <PokemonCard/>
+    </div>
+  )
+}
