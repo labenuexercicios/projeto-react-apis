da37ddff97e2688ee203ab29e9ad3c90d544b7e9
danilogmarques <danilogustavomarques@gmail.com>
2023-02-16 12:45:16 -0300

    finalizando details page

---
+++ my-project-api/src/router/Codinator.js
@@ -0,0 +1,23 @@
+
+
+export const goToPokedexPage = (navigate, set) => {
+    navigate('/pokedex');
+    set(2)
+    
+    
+
+};
+
+
+
+export const goToDetailsPage = (navigate) => {
+    navigate('/detalhes');
+    
+};
+
+export const goToListPage = (navigate) => {
+    navigate('/');
+    
+};
+
+
