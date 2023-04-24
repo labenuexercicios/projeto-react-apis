83cda87f34cc25c0611cf3c7f20da94df6b25012
Pedro Henrique <pdrohrocha.m@gmail.com>
2023-04-24 15:45:17 -0300

    edit
    editei coisas dentro do projeto

---
+++ pokedex-project/src/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+
+
+
+
+
+
+export default function App () {
+  return(
+    <div>
+      
+    </div>
+  )
+}
---
+++ pokedex-project/src/componentes/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+
+
+export default function Header () {
+    
+    return (
+        <div>
+            <buton>
+                Pokedex
+            </buton>
+            
+        </div>
+    ) 
+}
