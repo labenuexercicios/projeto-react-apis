9b4704f5e49aba20f81a3d0710686ce132f73ddb
Cristian Viery <viery.guimaraes01@gmail.com>
2023-05-10 14:51:22 -0300

    nb

--- pokeapi/src/Components/PokemonCard/PokemonCard.js
+++ pokeapi/src/Components/PokemonCard/PokemonCard.js
@@ -2,17 +2,43 @@ import { CatchButton, Container, Containerimg, Pokeball, Pokemon, PokemonName, P
 import pokeball from '../../assets/pokebola.png'
 import { getTypes } from '../../utils/PokeType'
 import { getColors } from '../../utils/TypeColor'
+import { useState, useEffect } from 'react'
 
 export const PokeCard = (props) => {
 
   const types = props.pokemons.data.types.map((type) => getTypes(type.type.name));
   const color = props.pokemons.data.types.map((typecolor) => getColors(typecolor.type.name));
-  console.log(color)
+  
+  const [captured, setCaptured] = useState(false);
+
+  useEffect(() => {
+    const capturedPokemon = JSON.parse(localStorage.getItem('capturedPokemon'));
+    if (capturedPokemon && capturedPokemon.includes(props.pokemons.data.id)) {
+      setCaptured(true);
+    }
+  }, [props.pokemons.data.id]);
+
+  const capturePokemon = (pokemon) => {
+    let capturedPokemon = JSON.parse(localStorage.getItem('capturedPokemon'));
+    if (!Array.isArray(capturedPokemon)) {
+      capturedPokemon = [];
+    }
+    capturedPokemon.push(pokemon.id);
+    localStorage.setItem('capturedPokemon', JSON.stringify(capturedPokemon));
+    setCaptured(true);
+  };
+
+  const removePokemon = (pokemon) => {
+    let capturedPokemon = JSON.parse(localStorage.getItem('capturedPokemon'));
+    capturedPokemon = capturedPokemon.filter(id => id !== pokemon.id);
+    localStorage.setItem('capturedPokemon', JSON.stringify(capturedPokemon));
+    setCaptured(false);
+  };
   
   return (
     <Container color={color}>
       <div>
-        <PokemonNumber></PokemonNumber>
+        <PokemonNumber>{props.pokemons.data.id}</PokemonNumber>
         <PokemonName>{props.pokemons.data.name}</PokemonName>
         <TypesContainer>
           {types.map((typeUrl) => (
@@ -23,7 +49,10 @@ export const PokeCard = (props) => {
       </div>
       <Containerimg>
         {<Pokemon src={`${props.pokemons.data.sprites.front_default}`} alt="" />}
-        <CatchButton>Capturar!</CatchButton>
+        {props.page === 'pokedex' ?
+          <CatchButton onClick={() => removePokemon(props.pokemons.data)}>Remover</CatchButton> :
+          <CatchButton onClick={() => capturePokemon(props.pokemons.data)}>Capturar!</CatchButton>
+        }
       </Containerimg>
       <Pokeball src={pokeball} alt="pokeball" />
     </Container>
--- pokeapi/src/Pages/Pokedex/PokePage.js
+++ pokeapi/src/Pages/Pokedex/PokePage.js
@@ -1,12 +1,36 @@
-import react, { useState } from 'react'
 import { Header } from '../../Components/Header/Header'
+import { useEffect, useState } from 'react';
 import { PokeCard } from '../../Components/PokemonCard/PokemonCard';
 
+export const Pokedex = ({ capturedPokemon = [] }) => {
+  const [pokemonList, setPokemonList] = useState(capturedPokemon);
 
-export const Pokedex =({capturedPokemon})=>{
-    return(
-        <div>
-      <Header/>
-    </div>
+  useEffect(() => {
+    const pokemon = localStorage.getItem('capturedPokemon');
+    if (pokemon) {
+      setPokemonList(JSON.parse(pokemon));
+    }
+  }, []);
+
+  const removePokemon = (id) => {
+    const filteredList = pokemonList.filter((pokemon) => pokemon.id !== id);
+    setPokemonList(filteredList);
+    localStorage.setItem('capturedPokemon', JSON.stringify(filteredList));
+  };
+  console.log(pokemonList)
+  return (
+    <div>
+        <Header />
+    <ul>
+      {pokemonList.map((pokemon) => (
+        <PokeCard
+          pokemons={{ data: pokemon }}
+          key={pokemon.id}
+          captured={true}
+          onRemove={() => removePokemon(pokemon.id)}
+        />
+      ))}
+    </ul>
+  </div>
   );
-}
+};
