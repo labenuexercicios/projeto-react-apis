94180bfa7739c9a8fb7484c737764cc4d04b866f
brenomorp <brenomoreirapro@gmail.com>
2023-05-14 15:24:49 -0300

    fix lottie dependency error

--- pokedex/src/components/LoadingScreen.js
+++ pokedex/src/components/LoadingScreen.js
@@ -1,30 +1,10 @@
-import { useState } from 'react';
-import Lottie from 'react-lottie';
+import Lottie from 'lottie-react';
 import animationData from '/public/animations/pokeball-loading.json';
 
 const LoadingScreen = () => {
-    const [animationState, setAnimationState] = useState({
-        isStopped: false,
-        isPaused: false,
-    });
-    const defaultOptions = {
-        loop: true,
-        autoplay: true,
-        animationData: animationData,
-        rendererSettings: {
-            preserveAspectRatio: 'xMidYMid slice',
-        },
-    };
-
     return (
         <div className="pointer-events-none absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-            <Lottie
-                options={defaultOptions}
-                height={400}
-                width={400}
-                isStopped={animationState.isStopped}
-                isPaused={animationState.isPaused}
-            />
+            <Lottie animationData={animationData} loop={true} />
         </div>
     );
 };
