d46d8658d4fb49220d1f9093bc662d095a1ac914
brenomorp <brenomoreirapro@gmail.com>
2023-05-19 19:05:26 -0300

    file restructuring

--- pokedex/src/constants/api.js
+++
@@ -1,2 +0,0 @@
-export const limit = '20';
-export const BASE_URL = 'https://pokeapi.co/api/v2';
---
+++ src/constants/api.js
@@ -0,0 +1,2 @@
+export const DEFAULT_LIMIT = '15';
+export const BASE_URL = 'https://pokeapi.co/api/v2/pokemon';
